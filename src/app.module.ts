5c57440998c1d832fc48ba129740eb61de656cb6
zefronner <muhammadrasulshixnazarov>
2025-06-19 13:26:39 +0500

    BronHotel loyihasi

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [ConfigModule.forRoot({
+    envFilePath: '.env',
+    isGlobal: true
+  }),
+TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: process.env.PG_HOST,
+  port: Number(process.env.PG_PORT),
+  username: process.env.PG_USER,
+  password: process.env.PG_PASS,
+  database: process.env.PG_DB,
+  synchronize: true,
+  autoLoadEntities: true,
+  entities: []
+}),
+]
+})
+export class AppModule {}
